1a91932c676e3f97b1be5bea40393e3e6bb6e336
João Paulo Sena <joaopaulo761@gmail.com>
2020-06-15 19:47:38 -0300

    :rocket: Release version ready

--- app/src/main/java/dev/forcetower/likesview/LikeApp.kt
+++ app/src/main/java/dev/forcetower/likesview/LikeApp.kt
@@ -2,6 +2,7 @@ package dev.forcetower.likesview
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import dev.forcetower.likesview.utils.crashlytics.CrashlyticsTree
 import timber.log.Timber
 
 @HiltAndroidApp
@@ -11,6 +12,8 @@ class LikeApp : Application() {
         // "Every time you log in production, a puppy dies"
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
         }
     }
 }
