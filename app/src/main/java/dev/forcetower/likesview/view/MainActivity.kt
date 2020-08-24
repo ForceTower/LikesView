3917a86960281d5bb856b2b18277dee996ff9142
João Sena <joaopaulo761@gmail.com>
2020-08-24 16:52:56 -0300

    Stuff about toolkit and deprecation

--- app/src/main/java/dev/forcetower/likesview/view/MainActivity.kt
+++ app/src/main/java/dev/forcetower/likesview/view/MainActivity.kt
@@ -2,6 +2,9 @@ package dev.forcetower.likesview.view
 
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -20,10 +23,7 @@ class MainActivity : BaseActivity() {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.root.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 
     override fun showSnack(string: String, duration: Int) {
