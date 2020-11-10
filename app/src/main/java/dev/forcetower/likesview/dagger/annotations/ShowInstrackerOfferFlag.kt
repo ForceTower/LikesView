469cb652df7d31d9ba19254afa7ea6cbe82356bc
João Sena <joaopaulo761@gmail.com>
2020-11-10 13:35:43 -0300

    Partner app offer

---
+++ app/src/main/java/dev/forcetower/likesview/dagger/annotations/ShowInstrackerOfferFlag.kt
@@ -0,0 +1,8 @@
+package dev.forcetower.likesview.dagger.annotations
+
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class ShowInstrackerOfferFlag
--- app/src/main/java/dev/forcetower/likesview/view/home/single/HomeSingleFragment.kt
+++ app/src/main/java/dev/forcetower/likesview/view/home/single/HomeSingleFragment.kt
@@ -1,11 +1,16 @@
 package dev.forcetower.likesview.view.home.single
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.core.os.postDelayed
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
@@ -14,10 +19,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.forcetower.likesview.R
 import dev.forcetower.likesview.core.model.database.InstagramProfile
+import dev.forcetower.likesview.dagger.annotations.ShowInstrackerOfferFlag
 import dev.forcetower.likesview.databinding.FragmentHomeSingleBinding
 import dev.forcetower.likesview.view.home.AddProfileAdapter
 import dev.forcetower.likesview.view.home.HomeViewModel
 import dev.forcetower.likesview.view.home.ReelsAdapter
+import dev.forcetower.likesview.view.instracker.InstrackerFragment
 import dev.forcetower.likesview.view.profile.HeaderAdapter
 import dev.forcetower.likesview.view.profile.MediaAdapter
 import dev.forcetower.likesview.view.profile.ProfileViewModel
@@ -27,6 +34,7 @@ import dev.forcetower.toolkit.lifecycle.EventObserver
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeSingleFragment : BaseFragment() {
@@ -34,6 +42,12 @@ class HomeSingleFragment : BaseFragment() {
     private lateinit var binding: FragmentHomeSingleBinding
     private val viewModel by activityViewModels<HomeViewModel>()
     private val profileViewModel by activityViewModels<ProfileViewModel>()
+    @Inject
+    @JvmField
+    @ShowInstrackerOfferFlag
+    var showInstrackingOffer: Boolean = true
+    @Inject
+    lateinit var preferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,6 +65,10 @@ class HomeSingleFragment : BaseFragment() {
         val add = AddProfileAdapter(viewModel)
         val reels = ReelsAdapter(viewModel)
         val merge = ConcatAdapter(add, reels)
+
+        if (savedInstanceState == null) {
+            prepareShowNewAppOffer()
+        }
 
         binding.run {
             recyclerReels.run {
@@ -138,5 +156,22 @@ class HomeSingleFragment : BaseFragment() {
                 }
             }
         }
+    }
+
+    private fun prepareShowNewAppOffer() {
+        val applications = requireContext().packageManager.getInstalledApplications(0)
+        val installed = applications.any { it.packageName == "dev.forcetower.instracker" }
+        if (installed) return
+
+        val count = preferences.getInt("instracker_offer_count", 0)
+        if (count % 3 == 0 && showInstrackingOffer) {
+            Handler(Looper.getMainLooper()).postDelayed(1500L) {
+                if (lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                    val dialog = InstrackerFragment()
+                    dialog.show(childFragmentManager, "instracker_offer")
+                }
+            }
+        }
+        preferences.edit().putInt("instracker_offer_count", count + 1).apply()
     }
 }
